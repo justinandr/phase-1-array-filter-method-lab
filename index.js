8c7fd454045a51a44c7ba4c9f0ba8d94a8853937
Justin Rodriguez <justin.r@me.com>
2023-05-24 13:30:30 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, name){
+    return array.filter(function(match){
+        return name.toLowerCase() === match.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, name){
+    return array.filter(function(match){
+        return match.startsWith(name)
+    })
+}
+
+function matchName(array, name){
+    return array.filter(function(match){
+        return match.name === name
+    })
+} 
